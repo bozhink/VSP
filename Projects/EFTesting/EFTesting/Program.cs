bf6128169716f6fa2cc00953d0666829ca7fd823
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-11 14:35:11 +0200

    Added EFTesting.

---
+++ Projects/EFTesting/EFTesting/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace EFTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new TelerikAcademyEntities())
+            {
+                Expression<Func<Project, bool>> expression = pr => pr.ProjectID == 1;
+
+                Console.WriteLine(expression.Body);
+
+                Func<Project, bool> function = pr => pr.ProjectID == 1;
+
+                var project = db.Projects
+                    //.Where(pj => pj.ProjectID == 1)
+                    //.FirstOrDefault();
+                    //.FirstOrDefault(pj => pj.ProjectID == 1);
+                    .FirstOrDefault(expression);
+
+                Console.WriteLine(project.Name);
+
+                // var p = db.Projects.Find(1);
+
+                var list = new List<Project>();
+
+                //list.FirstOrDefault(pr => pr.ProjectID == 1);
+                list.FirstOrDefault(function);
+
+                var town = new Town
+                {
+                    Name = "Sofia from console"
+                };
+
+                db.Towns.Add(town);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Projects/EFTesting/EFTesting/xx.cs
@@ -0,0 +1,21 @@
+﻿
+public class Rootobject
+{
+    public Class1[] Property1 { get; set; }
+}
+
+public class Class1
+{
+    public int Year { get; set; }
+    public int TransmissionType { get; set; }
+    public string ManufacturerName { get; set; }
+    public string Model { get; set; }
+    public float Price { get; set; }
+    public Dealer Dealer { get; set; }
+}
+
+public class Dealer
+{
+    public string Name { get; set; }
+    public string City { get; set; }
+}
