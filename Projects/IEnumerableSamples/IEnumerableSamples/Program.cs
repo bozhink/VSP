fbdab260b03837a342bacb32e24acbb5f2c2d4bc
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-11 14:37:19 +0200

    Added IEnumerableSamples.

---
+++ Projects/IEnumerableSamples/IEnumerableSamples/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IEnumerableSamples
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test_HashSetItemsAreUnique();
+        }
+
+        private static void Test_HashSetItemsAreUnique()
+        {
+            var stringList = new List<string>();
+            stringList.Add("a");
+            stringList.Add("a");
+            stringList.Add("a");
+            stringList.Add("b");
+            stringList.Add("b");
+            stringList.Add("b");
+            stringList.Add("c");
+            stringList.Add("c");
+            stringList.Add("c");
+
+            Console.WriteLine(string.Join(", ", stringList));
+
+            var hashSet = new HashSet<string>(stringList);
+            Console.WriteLine(string.Join(", ", hashSet));
+        }
+    }
+}
---
+++ Projects/IEnumerableSamples/IEnumerableSamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IEnumerableSamples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IEnumerableSamples")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cad40740-ce6e-4a2f-b9ed-1a98926794b3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
