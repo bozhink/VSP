0afd8acc88292df74b2133985f6589c95e8af3e1
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-11 14:33:25 +0200

    Added AsyncCS20150905.

---
+++ Projects/AsyncCS20150905/AsyncCS20150905/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncCS20150905
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Parallel.For(0, 100, (index, loopState) =>
+            ////{
+            ////    //if (index > loopState.LowestBreakIteration)
+            ////    //{
+            ////    //    return;
+            ////    //}
+
+            ////    if (index == 10)
+            ////    {
+            ////        loopState.Break();
+            ////    }
+            ////    //loopState.Stop();
+            ////    Console.WriteLine(index);
+            ////});
+
+
+            //Dictionary<string, string> s = new Dictionary<string, string>();
+            ConcurrentBag<string> cb = new ConcurrentBag<string>();
+            cb.Add("xx");
+
+            /*
+             * Deadlock
+             */
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                lock ("first")
+                {
+                    Console.WriteLine("A");
+                    lock ("second")
+                    {
+                        Console.WriteLine("B");
+                    }
+                }
+                Console.WriteLine("Exit 1");
+            });
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                lock ("second")
+                {
+                    Console.WriteLine("b");
+                    lock ("first")
+                    {
+                        Console.WriteLine("a");
+                    }
+                }
+                Console.WriteLine("Exit 2");
+            });
+
+            while(true)
+            {
+
+            }
+        }
+    }
+}
---
+++ Projects/AsyncCS20150905/TaskTests/Program.cs
@@ -0,0 +1,121 @@
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading;
+////using System.Threading.Tasks;
+
+////namespace SuareRootsAsync
+////{
+////    class Program
+////    {
+////        static Dictionary<int, double> GetSuareRoots(List<int> numbers)
+////        {
+////            Dictionary<int, double> qrTabes = new Dictionary<int, double>();
+////            foreach (int number in numbers)
+////            {
+////                Thread.Sleep(500);
+////                qrTabes[number] = Math.Sqrt(number);
+////            }
+
+////            return qrTabes;
+////        }
+
+////        static Task<Dictionary<int, double>> GetSuareRootsAsync(List<int> numbers)
+////        {
+////            Task<Dictionary<int, double>>  sq = Task.Run(() => GetSuareRoots(numbers));
+
+////            return sq;
+////        }
+
+////        static void PrintSquareRootsAsync(List<int> numbers)
+////        {
+////            GetSuareRootsAsync(numbers).ContinueWith(
+////                (resultTask) =>
+////                {
+////                    if (resultTask.IsFaulted) // error status
+////                    {
+////                        Console.WriteLine(resultTask.Exception);
+////                    }
+
+////                    foreach (var item in resultTask.Result)
+////                    {
+////                        Console.WriteLine(item.Key + " " + item.Value);
+////                    }
+////                }
+////                );
+////        }
+
+////        static void Main(string[] args)
+////        {
+////            //Task.Run();
+
+////            PrintSquareRootsAsync(Enumerable.Range(0, 10000).ToList());
+
+////            Console.WriteLine("done");
+
+
+////                Console.ReadLine();
+////        }
+////    }
+////}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuareRootsAsync
+{
+    class Program
+    {
+        static Dictionary<int, double> GetSuareRoots(List<int> numbers)
+        {
+            Dictionary<int, double> qrTabes = new Dictionary<int, double>();
+            foreach (int number in numbers)
+            {
+                Thread.Sleep(10);
+                qrTabes[number] = Math.Sqrt(number);
+            }
+
+            return qrTabes;
+        }
+
+        static Task<Dictionary<int, double>> GetSuareRootsAsync(List<int> numbers)
+        {
+            Task<Dictionary<int, double>> sq = Task.Run(() => GetSuareRoots(numbers));
+
+            return sq;
+        }
+
+        static async void PrintSquareRootsAsync(List<int> numbers)
+        {
+            try
+            {
+                Dictionary<int, double> table = await GetSuareRootsAsync(numbers);
+
+                foreach (var entry in table)
+                {
+                    Console.WriteLine(entry.Key + " " + entry.Value);
+                }
+            }
+            catch (Exception)
+            {
+                // This works.
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Task.Run();
+
+            PrintSquareRootsAsync(Enumerable.Range(0, 10).ToList());
+
+            Console.WriteLine("done");
+
+
+            Console.ReadLine();
+        }
+    }
+}
