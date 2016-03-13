4c78ae3a76e6fd80d7d9b1f6707885a6471adfc8
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-13 15:25:41 +0200

    XmlTests: Runner.Program is included.

---
+++ Projects/XmlTests/Runner/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Runner
+{
+    using System;
+    using System.Xml;
+
+    using XmlDomManager;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args.Length < 1)
+            {
+                Environment.Exit(1);
+            }
+
+            string inputFileName = args[0];
+            XmlDocument document = new XmlDocument
+            {
+                PreserveWhitespace = true
+            };
+
+            try
+            {
+                document.Load(inputFileName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Environment.Exit(2);
+            }
+
+            var tagger = new XmlTagger();
+
+            tagger.WrapTextInTag(document, "xx", "x");
+
+            Console.WriteLine(document.OuterXml);
+        }
+    }
+}
